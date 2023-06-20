831d2417b799fd5c5d264ebd7cce286f2e38c697
moyui <gxy1510@gmail.com>
2023-06-21 01:20:02 +0800

    chore: 构建规则

---
+++ src/app/boot/index.tsx
@@ -0,0 +1,15 @@
+"use client"
+import React from "react"
+import { Layout } from "antd"
+
+const { Header, Content, Footer, Sider } = Layout
+
+function Boot() {
+  return (
+    <Layout>
+      <Content></Content>
+    </Layout>
+  )
+}
+
+export default Boot
--- src/app/greet.tsx
+++ src/app/greet.tsx
@@ -1,11 +1,11 @@
-'use client'
+"use client"
 
-import { useEffect } from 'react'
-import { invoke } from '@tauri-apps/api/tauri'
+import { useEffect } from "react"
+import { invoke } from "@tauri-apps/api/tauri"
 
 export default function Greet() {
   useEffect(() => {
-    invoke<string>('greet', { name: 'Next.js' })
+    invoke<string>("greet", { name: "Next.js" })
       .then(console.log)
       .catch(console.error)
   }, [])
